34507c5bead39d362b709ff160f3cf54cc3a9f50
Michael Maurer <mjmaurer777@gmail.com>
2022-11-25 17:56:29 -0500

    fix pomrpts

--- external_prompts.py
+++ external_prompts.py
@@ -13,12 +13,12 @@ image_prompts = {
     # 0:['ImagePromptsWorkButArentVeryGood.png:2',],
 }
 # main_prompt = f"green forest and trees, {main_style}"
-animation_prompts = {
-    0: main_prompt,
-    anim_args_dict["switch_frame"] + 24 * 15: f"a massive root system, {main_style}",
-    anim_args_dict["switch_frame"]
-    + 24 * 30: f"the gears of nature turning in a forest, {main_style}",
-    # 20: "a beautiful banana, trending on Artstation",
-    # 30: "a beautiful coconut, trending on Artstation",
-    # 40: "a beautiful durian, trending on Artstation",
-}
+# animation_prompts = {
+#     0: main_prompt,
+#     anim_args_dict["switch_frame"] + 24 * 15: f"a massive root system, {main_style}",
+#     anim_args_dict["switch_frame"]
+#     + 24 * 30: f"the gears of nature turning in a forest, {main_style}",
+#     # 20: "a beautiful banana, trending on Artstation",
+#     # 30: "a beautiful coconut, trending on Artstation",
+#     # 40: "a beautiful durian, trending on Artstation",
+# }
