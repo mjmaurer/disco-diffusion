6b99b5229691916cbca5da51966cb02c54961bf0
Michael Maurer <mjmaurer777@gmail.com>
2022-11-26 17:19:24 -0500

    lower steps

--- disco.py
+++ disco.py
@@ -2593,7 +2593,7 @@ flow_padding_mode = "reflect"  # @param ['reflect','edge','wrap']
 # relative to image size, in range 0-1
 warp_interp = PIL.Image.LANCZOS  # TODO change this wherever PIL.Image.XX used
 batch_name = vid_input.split(".")[0]  # @param{type: 'string'}
-steps = 200  # @param [25,50,100,150,250,500,1000]{type: 'raw', allow-input: true}
+steps = 140  # @param [25,50,100,150,250,500,1000]{type: 'raw', allow-input: true}
 width_height_for_512x512_models = [1024, 576]  # @param{type: 'raw'}
 clip_guidance_scale = 15000  # @param{type: 'number'}
 tv_scale = 10000  # @param{type: 'number'}
