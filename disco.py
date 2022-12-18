aedf56978e7be3eb6c07c7e77a9719dbe04a173e
Michael Maurer <mjmaurer777@gmail.com>
2022-12-18 03:09:52 -0500

    skip steps

--- disco.py
+++ disco.py
@@ -2615,7 +2615,7 @@ skip_augs = False  # @param{type: 'boolean'}
 init_image = "/notebooks/output/_short2/_997_12_17__23_27/_short2(0)_0499.png"  # @param{type: 'string'}
 # Init scale and CGS must be balanced against each other
 init_scale = 10000  # @param{type: 'integer'}
-skip_steps = math.floor(steps * .9)  # @param{type: 'integer'}
+skip_steps = math.floor(steps * .83)  # @param{type: 'integer'}
 # @markdown *Make sure you set skip_steps to ~50% of your steps if you want to use an init image.*
 
 # @markdown ####**Image dimensions to be used for 256x256 models (e.g. pixelart models):**
@@ -2637,7 +2637,7 @@ start_frame = 24 * 6
 # eta = f"0:(0.01), {24 * 5}:(0.01), {target_frame}: (0.5)"  # @param ['40%', '50%', '60%', '70%', '80%'] {type: 'string'}
 # frames_skip_steps = f"0:(0.999), {start_frame}: (.999), {target_frame}: (0.65)"  # @param ['40%', '50%', '60%', '70%', '80%'] {type: 'string'}
 # blend_ramp = f"0:(1), {start_frame}: (1), {target_frame}: (4.5)"  # @param ['40%', '50%', '60%', '70%', '80%'] {type: 'string'}
-frames_skip_steps = f"0:(.9), 60:(0.7)"
+frames_skip_steps = f"0:(.83), 20:(0.7)"
 blend_ramp = f"0:(10)"
 eta = "0:(0.2)"
 flow_blend = "0:(.2)"  # @param {type:"string"}
