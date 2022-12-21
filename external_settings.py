352b3083ca7899ff8ac55a573a070462e0941bf7
Michael Maurer <mjmaurer777@gmail.com>
2022-12-21 11:25:10 -0500

    double tensor

--- external_settings.py
+++ external_settings.py
@@ -11,9 +11,11 @@ import numpy as np
 
 styled_seconds = 4
 ramp_seconds = 4
-strength_schedule = [0.6] + [.27] * (24 * styled_seconds) + list(np.linspace(0.27, 0, 24 * ramp_seconds))
-flow_blend_schedule = [0.999] * (24 * styled_seconds) + list(
-    np.linspace(0.999, 0.75, 24 * ramp_seconds)
+strength_schedule = (
+    [0.6] + [0.27] * (24 * styled_seconds) + list(np.linspace(0.27, 0, 24 * ramp_seconds))
+)
+flow_blend_schedule = [0.999] * (24 * (styled_seconds + ramp_seconds // 2)) + list(
+    np.linspace(0.999, 0, 24 * ramp_seconds / 2)
 )
 steps_schedule = [180]
 # flow_blend_schedule = list(np.linspace(0.999, 0.4, 24 * (seconds - 1))) + list(
