a86f9903c0e2c88f7a69e69120eb637721143396
Michael Maurer <mjmaurer777@gmail.com>
2022-12-20 21:25:19 -0500

    warp

--- external_settings.py
+++ external_settings.py
@@ -11,9 +11,9 @@ import numpy as np
 
 styled_seconds = 4
 ramp_seconds = 4
-strength_schedule = [0.52] + [.35] * (24 * styled_seconds) + list(np.linspace(0.35, 0, 24 * ramp_seconds))
+strength_schedule = [0.6] + [.27] * (24 * styled_seconds) + list(np.linspace(0.27, 0, 24 * ramp_seconds))
 flow_blend_schedule = [0.999] * (24 * styled_seconds) + list(
-    np.linspace(0.999, 0.5, 24 * ramp_seconds)
+    np.linspace(0.999, 0.75, 24 * ramp_seconds)
 )
 steps_schedule = [180]
 # flow_blend_schedule = list(np.linspace(0.999, 0.4, 24 * (seconds - 1))) + list(
