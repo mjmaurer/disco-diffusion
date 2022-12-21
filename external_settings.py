1f64ead2852528ef2faede5106beadf040ef778c
Michael Maurer <mjmaurer777@gmail.com>
2022-12-20 19:08:50 -0500

    schedules

--- external_settings.py
+++ external_settings.py
@@ -1,4 +1,4 @@
-vid_input = "_94archtrim.mp4"
+vid_input = "_93archtrimlonger.mp4"
 
 michael_mode = True
 
@@ -9,11 +9,16 @@ sd_model_path = "/storage/deforumsd/models/sd-v1-4.ckpt"  # @param {'type':'stri
 import math
 import numpy as np
 
-seconds = 7
-strength_schedule = [0.52] + list(np.linspace(0.3, 0, 24 * seconds))
+styled_seconds = 4
+ramp_seconds = 4
+strength_schedule = [0.52] * (24 * styled_seconds) + list(np.linspace(0.5, 0, 24 * ramp_seconds))
+flow_blend_schedule = [0.999] * (24 * styled_seconds) + list(
+    np.linspace(0.999, 0, 24 * ramp_seconds)
+)
 steps_schedule = [180]
-flow_blend_schedule = [0.999] 
 # flow_blend_schedule = list(np.linspace(0.999, 0.4, 24 * (seconds - 1))) + list(
 #     np.linspace(0.4, 0, 24 * 1)
 # )
-turbo_steps_schedule = [math.ceil(n) for n in list(np.linspace(5, 0.01, 24 * (seconds - 1)))]
+turbo_steps_schedule = [
+    math.ceil(n) for n in list(np.linspace(5, 0.01, 24 * (styled_seconds + ramp_seconds - 1)))
+]
