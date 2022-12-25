e79bf516024fef2535b1e145d9a0655b5d3cc316
Michael Maurer <mjmaurer777@gmail.com>
2022-12-24 21:20:57 -0500

    [mods] Support functions in keyframes

--- disco_utils.py
+++ disco_utils.py
@@ -44,64 +44,37 @@ def wget(url, outputdir):
     print(res)
 
 
-def get_inbetweens(key_frames, integer=False, max_frames=5000, interp_spline="Linear"):
-    """Given a dict with frame numbers as keys and a parameter value as values,
-    return a pandas Series containing the value of the parameter at every frame from 0 to max_frames.
-    Any values not provided in the input dict are calculated by linear interpolation between
-    the values of the previous and next provided frames. If there is no previous provided frame, then
-    the value is equal to the value of the next provided frame, or if there is no next provided frame,
-    then the value is equal to the value of the previous provided frame. If no frames are provided,
-    all frame values are NaN.
+def check_is_number(value):
+    import re
+    float_pattern = r"^(?=.)([+-]?([0-9]*)(\.([0-9]+))?)$"
+    return re.match(float_pattern, value)
 
-    Parameters
-    ----------
-    key_frames: dict
-        A dict with integer frame numbers as keys and numerical values of a particular parameter as values.
-    integer: Bool, optional
-        If True, the values of the output series are converted to integers.
-        Otherwise, the values are floats.
 
-    Returns
-    -------
-    pd.Series
-        A Series with length max_frames representing the parameter values for each frame.
+def get_inbetweens(key_frames, max_frames, integer=False, interp_method="Linear"):
+    import numexpr
 
-    Examples
-    --------
-    >>> max_frames = 5
-    >>> get_inbetweens({1: 5, 3: 6})
-    0    5.0
-    1    5.0
-    2    5.5
-    3    6.0
-    4    6.0
-    dtype: float64
-
-    >>> get_inbetweens({1: 5, 3: 6}, integer=True)
-    0    5
-    1    5
-    2    5
-    3    6
-    4    6
-    dtype: int64
-    """
     key_frame_series = pd.Series([np.nan for a in range(max_frames)])
 
-    for i, value in key_frames.items():
-        key_frame_series[i] = value
+    for i in range(0, max_frames):
+        if i in key_frames:
+            value = key_frames[i]
+            value_is_number = check_is_number(value)
+            # if it's only a number, leave the rest for the default interpolation
+            if value_is_number:
+                t = i
+                key_frame_series[i] = value
+        if not value_is_number:
+            t = i
+            key_frame_series[i] = numexpr.evaluate(value)
     key_frame_series = key_frame_series.astype(float)
-
-    interp_method = interp_spline
 
     if interp_method == "Cubic" and len(key_frames.items()) <= 3:
         interp_method = "Quadratic"
-
     if interp_method == "Quadratic" and len(key_frames.items()) <= 2:
         interp_method = "Linear"
 
     key_frame_series[0] = key_frame_series[key_frame_series.first_valid_index()]
     key_frame_series[max_frames - 1] = key_frame_series[key_frame_series.last_valid_index()]
-    # key_frame_series = key_frame_series.interpolate(method=intrp_method,order=1, limit_direction='both')
     key_frame_series = key_frame_series.interpolate(
         method=interp_method.lower(), limit_direction="both"
     )
@@ -142,7 +115,7 @@ def parse_key_frames(string, prompt_parser=None):
     """
     import re
 
-    pattern = r"((?P<frame>[0-9]+):[\s]*[\(](?P<param>[\S\s]*?)[\)])"
+    pattern = r"((?P<frame>[0-9]+):[\s]*\((?P<param>[\S\s]*?)\)([,][\s]?|[\s]?$))"
     frames = dict()
     for match_object in re.finditer(pattern, string):
         frame = int(match_object.groupdict()["frame"])
