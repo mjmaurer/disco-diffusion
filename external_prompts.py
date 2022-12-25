541b4cffe2e62349f7f322bc64f7cb18e7d15b72
Michael Maurer <mjmaurer777@gmail.com>
2022-12-25 11:55:23 -0500

    prompts

--- external_prompts.py
+++ external_prompts.py
@@ -17,35 +17,35 @@ other = (
 # woodcut?
 text_prompts = {
     "0": (
-        "A photograph, amazing   cinematic film still , concept art    laughing    cauliflower"
-        " Cthulhu  in a   farm ,  in  dawn,  art by  wes anderson  ,   RHADS ,  Beatrix potter ,"
-        " colorful,   Internal glow , large expressive eyes , large expressive eyes , large"
+        "A photograph, amazing cinematic film still , cartoon bear made of yarn in a lush forest "
+        " , in  dawn,  art by peter max , RHADS , "
+        " colorful, psychedelic , Internal glow , large expressive eyes , large expressive eyes , large"
         " expressive eyes ,  4k ,  subsurface scattering  8k, 4k, hd, intricate and highly"
-        " realistic, trending on art station, wide range of colors, orange, blue, photorealistic,"
+        " realistic, trending on art station, wide range of colors, green , blue, photorealistic,"
         " dramatic shadows, photorealistic, dramatic shadows, highly detailed, hyperreal, long"
         " view, vista"
     ),
-    "100": (
-        "A photograph, remarkable   oil painting ,  greek marble statue    hysterical    Tardigrade"
-        "  in a   world made of skittles ,  in  dawn,  art by  Moebius Jean Giraud ,   Salvador"
-        " Dali ,  Alejandro Jodorowsky ,   redshift render ,  Volumetric ,  raytracing ,  realistic"
-        "  8k, 4k, hd, intricate and highly realistic, trending on art station, photorealistic,"
-        " dramatic shadows, highly detailed, hyperreal, long view, vista"
-    ),
-    "200": (
-        "A photograph, amazing   daguerrotype ,  surrealist painting    raging    tsar bomba"
-        " explosion  in a   world made of skittles ,  in  evening ,  art by  Mark Ryden ,  Margaret"
-        " Keane ,  Jon Marro ,    intricate and highly detailed ,   Photorealistic ,  "
-        " Photorealistic,  Surreal  8k, 4k, hd, intricate and highly realistic, trending on art"
-        " station, photorealistic, dramatic shadows, highly detailed, hyperreal, long view, vista"
-    ),
-    "300": (
-        "A photograph, remarkable   cinematic film still ,  oil painting    planet scale   "
-        " Cerberus  in a   space suit  ,  in  dawn,  art by  Walt Disney ,  ross draws ,  MAX ERNST"
-        " ,   HDR ,  Studio light ,  Maximum texture ,   Photorealistic 8k, 4k, hd, intricate and"
-        " highly realistic, trending on art station, photorealistic, dramatic shadows, highly"
-        " detailed, hyperreal, long view, vista"
-    ),
+    # "100": (
+    #     "A photograph, remarkable   oil painting ,  greek marble statue    hysterical    Tardigrade"
+    #     "  in a   world made of skittles ,  in  dawn,  art by  Moebius Jean Giraud ,   Salvador"
+    #     " Dali ,  Alejandro Jodorowsky ,   redshift render ,  Volumetric ,  raytracing ,  realistic"
+    #     "  8k, 4k, hd, intricate and highly realistic, trending on art station, photorealistic,"
+    #     " dramatic shadows, highly detailed, hyperreal, long view, vista"
+    # ),
+    # "200": (
+    #     "A photograph, amazing   daguerrotype ,  surrealist painting    raging    tsar bomba"
+    #     " explosion  in a   world made of skittles ,  in  evening ,  art by  Mark Ryden ,  Margaret"
+    #     " Keane ,  Jon Marro ,    intricate and highly detailed ,   Photorealistic ,  "
+    #     " Photorealistic,  Surreal  8k, 4k, hd, intricate and highly realistic, trending on art"
+    #     " station, photorealistic, dramatic shadows, highly detailed, hyperreal, long view, vista"
+    # ),
+    # "300": (
+    #     "A photograph, remarkable   cinematic film still ,  oil painting    planet scale   "
+    #     " Cerberus  in a   space suit  ,  in  dawn,  art by  Walt Disney ,  ross draws ,  MAX ERNST"
+    #     " ,   HDR ,  Studio light ,  Maximum texture ,   Photorealistic 8k, 4k, hd, intricate and"
+    #     " highly realistic, trending on art station, photorealistic, dramatic shadows, highly"
+    #     " detailed, hyperreal, long view, vista"
+    # ),
 }
 
 negative_prompts = {0: ["red, face, humans, people, text, blurry, unfocused"]}
