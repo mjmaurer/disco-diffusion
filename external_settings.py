e38d56a74a21b2cab77181416e526042b0b4d49c
Michael Maurer <mjmaurer777@gmail.com>
2022-12-30 11:14:01 -0500

    seed and str

--- external_settings.py
+++ external_settings.py
@@ -21,7 +21,7 @@ strength_schedule = (
     [0.75] + [0.35] * (24 * styled_seconds) + list(np.linspace(0.35, 0.01, 24 * ramp_seconds))
 )
 psych_poster_seed = 245114
-seed = -1 # 1261233236 #660008352 #925432632 # 245114 # random.random() * 1000000
+seed = 1110122274 # 1261233236 #660008352 #925432632 # 245114 # random.random() * 1000000
 flow_blend_schedule = (
     [0.9] * (24 * styled_seconds) # .999 before
     + list(np.linspace(0.9, 0.55, 24 * (ramp_seconds - 1)))
