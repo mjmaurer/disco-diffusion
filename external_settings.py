f51933bbf5a9abe382a00001d9435c43cb476e16
Michael Maurer <mjmaurer777@gmail.com>
2023-01-02 17:48:23 -0500

    try

--- external_settings.py
+++ external_settings.py
@@ -1,7 +1,7 @@
-vid_input = "_90pinesdeforumintro.mp4"
-flow_vid_input = "/notebooks/init_images/_89timdance.mp4" 
+vid_input = "_89timdance.mp4"
+flow_vid_input = None # "/notebooks/init_images/_89timdance.mp4" 
 # 97 on stablewarp images_out has a good seed
-init_image = None # "/notebooks/deforum-output/deforum-texture/2022-12-31-15-13/20221231151334_01976.png"
+init_image = "/notebooks/deforum-output/deforum-texture/2022-12-31-15-13/20221231151334_01976.png"
 
 width = 1024 # 960 # 832 # 1024
 height = 576 # 512      # 576 
@@ -18,13 +18,12 @@ import random
 
 styled_seconds = 1
 ramp_seconds = 5
-strength_schedule = (
-    [0.2] 
-)
+strength_schedule = list(np.linspace(0, 0.2, 24))
+
 psych_poster_seed = 245114
 seed = random.random() * 1000000
 flow_blend_schedule = (
-    [0.95]
+    list(np.linspace(0, 0.95, 12))
     # [0.9] * (24 * styled_seconds) # .999 before
     # + list(np.linspace(0.9, 0.55, 24 * (ramp_seconds - 1)))
     # + list(np.linspace(0.55, 0, 24))
@@ -34,7 +33,7 @@ steps_schedule = [150]
 # flow_blend_schedule = list(np.linspace(0.999, 0.4, 24 * (seconds - 1))) + list(
 #     np.linspace(0.4, 0, 24 * 1)
 # )
-turbo_steps_schedule = [2]
+turbo_steps_schedule = [1, 2]
 # turbo_steps_schedule = [
 #     math.ceil(n) for n in list(np.linspace(5, 2.01, 24 * (styled_seconds + ramp_seconds - 1)))
 # ] + [
