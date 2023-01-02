505411e74d0409f7b31d68cfff9fbda9379f5c39
Michael Maurer <mjmaurer777@gmail.com>
2023-01-01 19:02:30 -0500

    vid

--- external_settings.py
+++ external_settings.py
@@ -1,9 +1,9 @@
-vid_input = "_93archtrimlonger.mp4"
+vid_input = "_91sarahclose.mp4"
 # 97 on stablewarp images_out has a good seed
 init_image = "/notebooks/images_out/_93archtrimlonger/_93archtrimlonger(115)_000001.png"
 
-width = 960 # 832 # 1024
-height = 512      # 576 
+width =  1024
+height = 576 
 
 michael_mode = True
 
@@ -18,17 +18,14 @@ import random
 styled_seconds = 1
 ramp_seconds = 5
 strength_schedule = (
-    [0.75] + [0.35] * (24 * styled_seconds) + list(np.linspace(0.35, 0.01, 24 * ramp_seconds))
+    [0.2]
 )
 psych_poster_seed = 245114
-seed = 1261233236 #660008352 #925432632 # 245114 # random.random() * 1000000
+seed = random.random() * 1000000
 flow_blend_schedule = (
-    [0.9] * (24 * styled_seconds) # .999 before
-    + list(np.linspace(0.9, 0.55, 24 * (ramp_seconds - 1)))
-    + list(np.linspace(0.55, 0, 24))
-    # + list(np.linspace(0.8, 0, 24))
+    [0.9]
 )
-steps_schedule = [150]
+steps_schedule = [120]
 # flow_blend_schedule = list(np.linspace(0.999, 0.4, 24 * (seconds - 1))) + list(
 #     np.linspace(0.4, 0, 24 * 1)
 # )
